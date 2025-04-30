0adbf856ccf53971bd4bde5c591d8128e293a2fc
J0a0p3dr067 <pjpedro158@gmail.com>
2025-04-30 09:33:41 -0300

    fazendo o home e suas resposividade

--- projetos/Portifolio-real/script.js
+++ projetos/Portifolio-real/script.js
@@ -1,10 +1,7 @@
 function clickMenu() {
-    if (menu.style.display == "block") {
-        menu.style.display = "none";
-    }
-    else {
-        menu.style.display = "block";
-    }
+    const menu = document.getElementById("menu");
+    const button = document.querySelector(".hamburger");
     
-    
+    menu.classList.toggle("active");
+    button.setAttribute("aria-expanded", menu.classList.contains("active"));
 }
